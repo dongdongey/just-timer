8b2ec07084f7c9569621252cf7f5d0fa93b890a3
dongdongey <110368754+dongdongey@users.noreply.github.com>
2024-04-06 11:55:11 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var timecount = 0
 var sec = 0
 var min = 0
-var sex = true
+var bool = true
 var btn = document.getElementById("btn1")
 btn.addEventListener('click', Time)
 btn.addEventListener('touchstart', Time_for_touchstart)
@@ -10,7 +10,7 @@ var timerId
 // timerId는 밖에서 선언해줬어야 함
 // 그래야 함수 안 어디서든 접근이 가능함
 function Time(){
-    if (sex === true) {
+    if (bool === true) {
         btn.innerText = `stop`
         timerId = setInterval(() => {
             timecount = timecount + 1
@@ -22,7 +22,7 @@ function Time(){
             }
             x.innerText=`${String(min).padStart(2, "0")}:${String(sec).padStart(2, "0")}`; 
         }, 1000) // setInterval은 변수에 대입하는 동시에 작동함
-        sex = false
+        bool = false
         btn.className='stopbtn'
     }
     else if (sex === false) {
@@ -30,7 +30,7 @@ function Time(){
         clearInterval(timerId)
         // clearInterval을 작동하면 재사용이 불가능함
         // 따라서 타이머를 다시 사용하려면 변수에 setInterval을 다시 대입해줘야함
-        sex = true
+        bool = true
         btn.className='startbtn'
     }
 }
